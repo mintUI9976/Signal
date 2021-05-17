e595b5d0cdc4fd1089c6a6dd4e53fb331bb77fad
mint9976 <niklas.griese@zyonic.de>
2021-05-17 13:40:11 +0200

    update to v0.5.6 patch version fix interval issues / cpu issues

--- src/main/java/com/zyonicsoftware/minereaper/signal/incoming/InputStreamThread.java
+++ src/main/java/com/zyonicsoftware/minereaper/signal/incoming/InputStreamThread.java
@@ -1,6 +1,6 @@
 package com.zyonicsoftware.minereaper.signal.incoming;
 
-import com.coreoz.wisp.schedule.Schedule;
+import com.coreoz.wisp.schedule.Schedules;
 import com.zyonicsoftware.minereaper.signal.buffer.ReadingByteBuffer;
 import com.zyonicsoftware.minereaper.signal.caller.SignalCallRegistry;
 import com.zyonicsoftware.minereaper.signal.caller.SignalCaller;
@@ -14,8 +14,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.net.Socket;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.time.Duration;
 import java.util.UUID;
 
 public class InputStreamThread {
@@ -23,8 +22,6 @@ public class InputStreamThread {
     private final Client client;
     private final Socket socket;
     private InputStream finalInputStream;
-    private final Timer timer;
-    private TimerTask timerTask;
     private final Class<? extends SignalCaller> signalCaller;
     private final String jobName = "incoming_packets_" + UUID.randomUUID().toString();
 
@@ -32,7 +29,6 @@ public class InputStreamThread {
         this.client = client;
         this.socket = this.client.getSocket();
         this.signalCaller = SignalCallRegistry.getReferenceCaller();
-        this.timer = new Timer(this.jobName);
     }
 
     public void run() {
@@ -42,91 +38,71 @@ public class InputStreamThread {
                 this.finalInputStream = this.socket.getInputStream();
             }
 
-            this.client.getScheduler().schedule(this.jobName, () -> this.timer.scheduleAtFixedRate(this.timerTask = new TimerTask() {
-                @Override
-                public void run() {
-                    try {
-                        if (InputStreamThread.this.socket.isClosed()) {
-                            //interrupt thread
-                            InputStreamThread.this.interrupt();
-                            return;
-                        }
-                        //check if finalInputStream is null
-                        if (InputStreamThread.this.finalInputStream.available() > 0) {
-                            final int b = InputStreamThread.this.finalInputStream.read();
-                            if (b != -1) {
-                                //check if byte array length smaller then 255 bytes
-                                if (b < 255) {
-                                    //this.bytes.set(new byte[b]);
-                                    //receive bytes
-                                    //this.finalInputStream.read(this.bytes.get(), 0, b);
-                                    final ReadingByteBuffer readingByteBuffer = new ReadingByteBuffer(InputStreamThread.this.finalInputStream.readNBytes(b));
-                                    //read packetId
-                                    final int packetId = readingByteBuffer.readInt();
+            this.client.getScheduler().schedule(this.jobName, () -> {
+                try {
+                    if (InputStreamThread.this.socket.isClosed()) {
+                        //interrupt thread
+                        InputStreamThread.this.interrupt();
+                        return;
+                    }
+                    //check if finalInputStream is null
+                    if (InputStreamThread.this.finalInputStream != null && InputStreamThread.this.finalInputStream.available() > 0) {
+                        final int b = InputStreamThread.this.finalInputStream.read();
+                        if (b != -1) {
+                            //check if byte array length smaller then 255 bytes
+                            if (b < 255) {
+                                //this.bytes.set(new byte[b]);
+                                //receive bytes
+                                //this.finalInputStream.read(this.bytes.get(), 0, b);
+                                final ReadingByteBuffer readingByteBuffer = new ReadingByteBuffer(InputStreamThread.this.finalInputStream.readNBytes(b));
+                                //read packetId
+                                final int packetId = readingByteBuffer.readInt();
 
-                                    //check if packet is UpdateUUIDPacket
-                                    if (packetId == -2) {
+                                //check if packet is UpdateUUIDPacket
+                                if (packetId == -2) {
+                                    //read connectionUUID
+                                    final UUID connectionUUID = readingByteBuffer.readUUID();
+                                    //set updated connectionUUID
+                                    InputStreamThread.this.client.getConnectionUUID().set(connectionUUID);
+                                } else {
+                                    //get packet
+                                    final Class<? extends Packet> packet = PacketRegistry.get(packetId);
+
+                                    // check if received packet not null
+                                    if (packet != null) {
                                         //read connectionUUID
                                         final UUID connectionUUID = readingByteBuffer.readUUID();
-                                        //set updated connectionUUID
-                                        InputStreamThread.this.client.getConnectionUUID().set(connectionUUID);
+                                        //initialise packet
+                                        packet.getDeclaredConstructor(UUID.class).newInstance(connectionUUID).receive(readingByteBuffer);
+                                        //SignalProvider.getSignalProvider().setIncomingPackets(SignalProvider.getSignalProvider().getIncomingPackets() + 1);
+                                        InputStreamThread.this.signalCaller.getDeclaredConstructor(String.class).newInstance(this.toString()).receivePacketMessage(SignalProvider.getSignalProvider().getIncomingPacketMessage());
                                     } else {
-                                        //get packet
-                                        final Class<? extends Packet> packet = PacketRegistry.get(packetId);
-
-                                        // check if received packet not null
-                                        if (packet != null) {
-                                            //read connectionUUID
-                                            final UUID connectionUUID = readingByteBuffer.readUUID();
-                                            //initialise packet
-                                            packet.getDeclaredConstructor(UUID.class).newInstance(connectionUUID).receive(readingByteBuffer);
-                                            //SignalProvider.getSignalProvider().setIncomingPackets(SignalProvider.getSignalProvider().getIncomingPackets() + 1);
-                                            InputStreamThread.this.signalCaller.getDeclaredConstructor(String.class).newInstance(this.toString()).receivePacketMessage(SignalProvider.getSignalProvider().getIncomingPacketMessage());
-                                        } else {
-                                            InputStreamThread.this.signalCaller.getDeclaredConstructor(String.class).newInstance(this.toString()).receivePacketIsNullMessage(SignalProvider.getSignalProvider().getIncomingPacketIsNull());
-                                        }
+                                        InputStreamThread.this.signalCaller.getDeclaredConstructor(String.class).newInstance(this.toString()).receivePacketIsNullMessage(SignalProvider.getSignalProvider().getIncomingPacketIsNull());
                                     }
-                                } else {
-                                    InputStreamThread.this.signalCaller.getDeclaredConstructor(String.class).newInstance(this.toString()).receiveLengthToLargeMessage(SignalProvider.getSignalProvider().getIncomingLengthToLarge());
                                 }
                             } else {
-                                //close socket
-                                InputStreamThread.this.signalCaller.getDeclaredConstructor(String.class).newInstance(this.toString()).receiveSocketCloseMessage(SignalProvider.getSignalProvider().getIncomingSocketCloseMessage());
-                                InputStreamThread.this.socket.close();
+                                InputStreamThread.this.signalCaller.getDeclaredConstructor(String.class).newInstance(this.toString()).receiveLengthToLargeMessage(SignalProvider.getSignalProvider().getIncomingLengthToLarge());
                             }
+                        } else {
+                            //close socket
+                            InputStreamThread.this.signalCaller.getDeclaredConstructor(String.class).newInstance(this.toString()).receiveSocketCloseMessage(SignalProvider.getSignalProvider().getIncomingSocketCloseMessage());
+                            InputStreamThread.this.socket.close();
                         }
-
-                    } catch (final InstantiationException | IllegalAccessException | NoSuchMethodException | InvocationTargetException | IOException ignored) {
-                        InputStreamThread.this.restart();
                     }
+
+                } catch (final InstantiationException | IllegalAccessException | NoSuchMethodException | InvocationTargetException | IOException exception) {
+                    throw new SignalException(SignalProvider.getSignalProvider().getInputStreamThrowsAnException(), exception);
                 }
-            }, 0, 1), Schedule.willNeverBeExecuted);
+            }, Schedules.fixedDelaySchedule(Duration.ofMillis(50)));
         } catch (final IOException exception) {
             throw new SignalException(SignalProvider.getSignalProvider().getInputStreamThrowsAnException(), exception);
         }
         //start reading byte arrays
     }
 
-    protected void restart() {
-        this.timerTask.cancel();
-        this.timer.purge();
-        this.client.getScheduler().findJob(this.jobName).ifPresent(job -> job.threadRunningJob().interrupt());
-        this.client.getScheduler().cancel(this.jobName).thenAccept(job -> {
-            try {
-                this.signalCaller.getDeclaredConstructor(String.class).newInstance(this.toString()).canceledJob(SignalProvider.getSignalProvider().getCanceledJobMessage().replaceAll("%job%", job.name()));
-            } catch (final InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException exception) {
-                throw new SignalException(SignalProvider.getSignalProvider().getCanceledJobThrowsAnException(), exception);
-            }
-        });
-        this.run();
-    }
-
     public void interrupt() {
         try {
             this.finalInputStream.close();
-            this.timerTask.cancel();
-            this.timer.purge();
-            this.timer.cancel();
             this.client.getScheduler().findJob(this.jobName).ifPresent(job -> job.threadRunningJob().interrupt());
             this.client.getScheduler().cancel(this.jobName).thenAccept(job -> {
                 try {
--- src/main/java/com/zyonicsoftware/minereaper/signal/outgoing/OutputStreamThread.java
+++ src/main/java/com/zyonicsoftware/minereaper/signal/outgoing/OutputStreamThread.java
@@ -1,6 +1,6 @@
 package com.zyonicsoftware.minereaper.signal.outgoing;
 
-import com.coreoz.wisp.schedule.Schedule;
+import com.coreoz.wisp.schedule.Schedules;
 import com.zyonicsoftware.minereaper.signal.buffer.WritingByteBuffer;
 import com.zyonicsoftware.minereaper.signal.caller.SignalCallRegistry;
 import com.zyonicsoftware.minereaper.signal.caller.SignalCaller;
@@ -15,15 +15,16 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.net.Socket;
-import java.util.*;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 public class OutputStreamThread {
 
     private final Client client;
     private final Socket socket;
     private final List<Packet> packets = new ArrayList<>();
-    private final Timer timer;
-    private TimerTask timerTask;
     private OutputStream finalOutputStream;
     private final Class<? extends SignalCaller> signalCaller;
     private final String jobName = "outgoing_packets_" + UUID.randomUUID().toString();
@@ -32,97 +33,74 @@ public class OutputStreamThread {
         this.client = client;
         this.socket = this.client.getSocket();
         this.signalCaller = SignalCallRegistry.getReferenceCaller();
-        this.timer = new Timer(this.jobName);
     }
 
     public void run() {
         //initialise outputStream
         try {
             this.finalOutputStream = this.socket.getOutputStream();
-
             //start sending send byte arrays
-            this.client.getScheduler().schedule(this.jobName, () -> this.timer.scheduleAtFixedRate(this.timerTask = new TimerTask() {
-                @Override
-                public void run() {
-                    try {
-                        if (OutputStreamThread.this.socket.isClosed()) {
-                            //interrupt thread
-                            OutputStreamThread.this.interrupt();
-                            return;
-                        }
-                        //skip when no packets available to send
-                        if (!OutputStreamThread.this.packets.isEmpty()) {
-                            //get next packet available to send
-                            final Packet packet = OutputStreamThread.this.packets.get(0);
-                            //check if packet is valid
-                            if (packet != null) {
-                                //remove packet
-                                OutputStreamThread.this.packets.remove(0);
-                                final WritingByteBuffer writingByteBuffer = new WritingByteBuffer();
-                                //check if packet is UpdateUUIDPacket
-                                if (packet.getClass().equals(UpdateUUIDPacket.class)) {
-                                    writingByteBuffer.writeInt(-2);
-                                    writingByteBuffer.writeUUID(packet.getConnectionUUID());
-                                } else {
-                                    //get packetId
-                                    final int packetId = PacketRegistry.indexOf(packet.getClass());
-                                    //write packetId
-                                    writingByteBuffer.writeInt(packetId);
-                                    //write connectionUuid
-                                    writingByteBuffer.writeUUID(OutputStreamThread.this.client.getConnectionUUID().get());
-                                    //initialise packet
-                                    packet.send(writingByteBuffer);
-                                }
-                                //receive bytes
-                                final byte[] bytes = writingByteBuffer.toBytes();
-                                if (bytes.length < 255) {
-                                    //check if outputstream is null
-                                    assert OutputStreamThread.this.finalOutputStream != null;
-                                    //write bytes length
-                                    OutputStreamThread.this.finalOutputStream.write(bytes.length);
-                                    //write bytes
-                                    OutputStreamThread.this.finalOutputStream.write(bytes);
-                                    //flush outputStream
-                                    OutputStreamThread.this.finalOutputStream.flush();
-                                    //SignalProvider.getSignalProvider().setOutgoingPackets(SignalProvider.getSignalProvider().getOutgoingPackets() + 1);
-                                    OutputStreamThread.this.signalCaller.getDeclaredConstructor(String.class).newInstance(this.toString()).sendPacketMessage(SignalProvider.getSignalProvider().getOutgoingPacketMessage());
-                                } else {
-                                    OutputStreamThread.this.signalCaller.getDeclaredConstructor(String.class).newInstance(this.toString()).sendLengthToLargeMessage(SignalProvider.getSignalProvider().getOutgoingLengthToLarge());
-                                }
+            this.client.getScheduler().schedule(this.jobName, () -> {
+                try {
+                    if (this.socket.isClosed()) {
+                        //interrupt thread
+                        this.interrupt();
+                        return;
+                    }
+                    //skip when no packets available to send
+                    if (this.finalOutputStream != null && !this.packets.isEmpty()) {
+                        //get next packet available to send
+                        final Packet packet = this.packets.get(0);
+                        //check if packet is valid
+                        if (packet != null) {
+                            //remove packet
+                            this.packets.remove(0);
+                            final WritingByteBuffer writingByteBuffer = new WritingByteBuffer();
+                            //check if packet is UpdateUUIDPacket
+                            if (packet.getClass().equals(UpdateUUIDPacket.class)) {
+                                writingByteBuffer.writeInt(-2);
+                                writingByteBuffer.writeUUID(packet.getConnectionUUID());
+                            } else {
+                                //get packetId
+                                final int packetId = PacketRegistry.indexOf(packet.getClass());
+                                //write packetId
+                                writingByteBuffer.writeInt(packetId);
+                                //write connectionUuid
+                                writingByteBuffer.writeUUID(this.client.getConnectionUUID().get());
+                                //initialise packet
+                                packet.send(writingByteBuffer);
+                            }
+                            //receive bytes
+                            final byte[] bytes = writingByteBuffer.toBytes();
+                            if (bytes.length < 255) {
+                                //check if outputstream is null
+                                assert this.finalOutputStream != null;
+                                //write bytes length
+                                this.finalOutputStream.write(bytes.length);
+                                //write bytes
+                                this.finalOutputStream.write(bytes);
+                                //flush outputStream
+                                this.finalOutputStream.flush();
+                                //SignalProvider.getSignalProvider().setOutgoingPackets(SignalProvider.getSignalProvider().getOutgoingPackets() + 1);
+                                this.signalCaller.getDeclaredConstructor(String.class).newInstance(this.toString()).sendPacketMessage(SignalProvider.getSignalProvider().getOutgoingPacketMessage());
+                            } else {
+                                this.signalCaller.getDeclaredConstructor(String.class).newInstance(this.toString()).sendLengthToLargeMessage(SignalProvider.getSignalProvider().getOutgoingLengthToLarge());
                             }
                         }
-                    } catch (final IOException | NullPointerException | InstantiationException | InvocationTargetException | NoSuchMethodException | IllegalAccessException exception) {
-                        OutputStreamThread.this.restart();
                     }
+                } catch (final IOException | NullPointerException | InstantiationException | InvocationTargetException | NoSuchMethodException | IllegalAccessException exception) {
+                    throw new SignalException(SignalProvider.getSignalProvider().getOutputStreamThrowsAnException(), exception);
                 }
-            }, 0, 1), Schedule.willNeverBeExecuted);
-
+            }, Schedules.fixedDelaySchedule(Duration.ofMillis(50)));
         } catch (final IOException exception) {
             throw new SignalException(SignalProvider.getSignalProvider().getOutputStreamThrowsAnException(), exception);
         }
 
     }
 
-    protected void restart() {
-        this.timerTask.cancel();
-        this.timer.purge();
-        this.client.getScheduler().findJob(this.jobName).ifPresent(job -> job.threadRunningJob().interrupt());
-        this.client.getScheduler().cancel(this.jobName).thenAccept(job -> {
-            try {
-                this.signalCaller.getDeclaredConstructor(String.class).newInstance(this.toString()).canceledJob(SignalProvider.getSignalProvider().getCanceledJobMessage().replaceAll("%job%", job.name()));
-            } catch (final InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException exception) {
-                throw new SignalException(SignalProvider.getSignalProvider().getCanceledJobThrowsAnException(), exception);
-            }
-        });
-        this.run();
-    }
-
     public void interrupt() {
         try {
             this.finalOutputStream.close();
-            this.timerTask.cancel();
-            this.timer.purge();
-            this.timer.cancel();
             this.client.getScheduler().findJob(this.jobName).ifPresent(job -> job.threadRunningJob().interrupt());
             this.client.getScheduler().cancel(this.jobName).thenAccept(job -> {
                 try {
